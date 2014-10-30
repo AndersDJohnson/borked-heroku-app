9a872f62830dbaae966115e788b6796f519f9c0f
Anders D. Johnson <adjohnson916@gmail.com>
2014-10-29 21:52:42 -0500

    use PORT env var

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/', function(req, res) {
   res.send('Success' + exclamations);
 });
 
-var port = 5000;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
